af36312905aa80b38cd0ca19dbea49ee8fc0b6fe
Daniel Muñoz <danielfmunozc@gmail.com>
2021-10-25 17:08:32 -0500

    segunda versión del código de python

--- python/estadistico.py
+++ python/estadistico.py
@@ -1,16 +1,36 @@
 # Programa estadístico básico
 import math
 
-print('''
-Bienvenido! en este programa podras calcular la media (y la media ponderada) de los números que ingreses
-''')
+def media():
+    pass
+def mediaP():
+    pass
 
-input('''
+
+
+def run():
+    print('''
+Bienvenido! en este programa podras calcular la media (y la media ponderada) de los números que ingreses
+    ''')
+    calculo = input('''
 Primero que todo elige que función del programa quieres usar:
 
 a) Media normal.
 b) Media ponderada.
 
-Cuál calculo deseas realizar:
-''')
+Cuál calculo deseas realizar: ''')
 
+    if calculo == 'a':
+        result = media()
+    elif calculo == 'b':
+        result = mediaP()
+    else:
+        print('\nPor favor elige una opción válida')
+
+
+
+if __name__ == '__main__':
+    run()
+
+
+#Continuará
