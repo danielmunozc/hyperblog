17d43dbc8af46cc38ae63b033786c72c33db273b
Daniel Muñoz <danielfmunozc@gmail.com>
2021-10-25 16:56:27 -0500

    Creación programa estadistico

---
+++ python/estadistico.py
@@ -0,0 +1,16 @@
+# Programa estadístico básico
+import math
+
+print('''
+Bienvenido! en este programa podras calcular la media (y la media ponderada) de los números que ingreses
+''')
+
+input('''
+Primero que todo elige que función del programa quieres usar:
+
+a) Media normal.
+b) Media ponderada.
+
+Cuál calculo deseas realizar:
+''')
+
